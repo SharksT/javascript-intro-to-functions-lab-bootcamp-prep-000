0094d9a37f16073a1a01bd0712b99629fc4afb4d
Felipe Passos <felipekaires@hotmail.com>
2017-10-16 22:31:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ function sayHiToGrandma(string)
 {
   if (string == string.toLowerCase())
   {
-      return "I can\'t hear you!" 
+      return "I can\'t hear you!"
   }
   else if (string == string.toUpperCase())
   {
-    
+    returns "YES INDEED!"
   }
 
 }
